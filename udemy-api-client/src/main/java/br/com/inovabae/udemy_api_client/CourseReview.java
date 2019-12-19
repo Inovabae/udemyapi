3c389945c29b675b4fae0540aa23e7d67e7266f2
Evandro Costa Neves <ecnjava@gmail.com>
2019-12-19 19:12:44 -0300

    Communicating throughout Udemy client

--- udemy-api-client/src/main/java/br/com/inovabae/udemy_api_client/CourseReview.java
+++ udemy-api-client/src/main/java/br/com/inovabae/udemy_api_client/CourseReview.java
@@ -1,5 +1,32 @@
 package br.com.inovabae.udemy_api_client;
 
 public class CourseReview {
+	private String _class;
+	private String id;
+	private Integer rating;
+
+	public String get_class() {
+		return _class;
+	}
+
+	public void set_class(String _class) {
+		this._class = _class;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public Integer getRating() {
+		return rating;
+	}
+
+	public void setRating(Integer rating) {
+		this.rating = rating;
+	}
 
 }
--- udemy-api-client/src/main/java/br/com/inovabae/udemy_api_client/UdemyApiClient.java
+++ udemy-api-client/src/main/java/br/com/inovabae/udemy_api_client/UdemyApiClient.java
@@ -1,5 +1,12 @@
 package br.com.inovabae.udemy_api_client;
 
+import java.io.StringReader;
+import java.util.List;
+
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonObject;
+import javax.json.JsonValue;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Invocation;
@@ -9,7 +16,6 @@ import javax.ws.rs.core.Response;
 
 import org.glassfish.jersey.client.ClientConfig;
 import org.glassfish.jersey.client.authentication.HttpAuthenticationFeature;
-
 
 public class UdemyApiClient {
 	public static void main(String[] args) {
@@ -24,12 +30,20 @@ public class UdemyApiClient {
 
 		Invocation.Builder invocationBuilder = webTarget.request(MediaType.APPLICATION_JSON);
 		Response response = invocationBuilder.get();
-		
-		CourseReview reviews = response.readEntity(CourseReview.class);
-		
+		String body = response.readEntity(String.class);
 
-		System.out.println(response.getEntity().toString());
-		System.out.println(response.getStatusInfo());
-		System.out.println(reviews.toString());
+		JsonObject jArray = Json.createReader(new StringReader(body)).readObject();
+		JsonArray jsonArray = jArray.getJsonArray("results");
+		for (int i = 0; i < jsonArray.size(); i++) {
+			JsonObject jArray2 = jsonArray.getJsonObject(i);
+			System.out.println(jArray2.getString("content"));
+		}
+
+		response.close();
+		// CourseReview reviews = response.readEntity(CourseReview.class);
+		//
+
+		// System.out.println(response.getEntity().toString());
+		// System.out.println(response.getStatusInfo());
 	}
 }
